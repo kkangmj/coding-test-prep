9f4986b95237527ab2295ee4aaeccc0d9032896c
minju <bbak0402dd@gmail.com>
2022-08-02 18:58:12 +0900

    [problem] 리트코드 234번 문제

---
+++ problem/leetcode/Solution_234.java
@@ -0,0 +1,45 @@
+/**
+ *  234. Palindrome Linked List
+ */
+
+import java.util.ArrayList;
+
+public class Solution_234 {
+
+  public boolean isPalindrome(ListNode head) {
+    ArrayList<Integer> nodes = new ArrayList<>();
+    ListNode node = head;
+
+    while (node != null) {
+      nodes.add(node.val);
+      node = node.next;
+    }
+
+    for (int i = 0; i < nodes.size(); i++) {
+      if (nodes.get(i) != nodes.get(nodes.size() - i - 1)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+}
